d285d4587388d915214de4b04c0f61a1eaf00801
Aldo Arias Figueroa <aldo.ariasfigueroa@kony.com>
2019-05-07 15:58:51 -0700

    Commiting changes to the UI

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text } from "react-native";
+import styled from "styled-components";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>This is my first App!</Text>
-      </View>
+      <Container>
+        <TitleBar>
+          <Avatar source={require("./assets/avatar.jpg")} />
+          <Title>Welcome back,</Title>
+          <Name>Aldo</Name>
+        </TitleBar>
+        <Subtitle>Continue Learning</Subtitle>
+      </Container>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f0f3f5',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Subtitle = styled.Text`
+  font-size: 15px;
+  color: #b8b3c3;
+  font-weight: 600;
+  margin-left: 20px;
+  margin-top: 50px;
+  text-transform: uppercase;
+`;
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #f0f3f5;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8b3c3;
+  font-weight: 500;
+`;
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 80px;
+`;
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+`;
